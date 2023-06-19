32e2159880fc4362d6f8cc54e97dbf20b0bb4237
Piyush <piyushbhattarai04@gmail.com>
2023-06-19 13:05:22 +0545

    added spring file

---
+++ src/main/java/com/springproject/model/User.java
@@ -0,0 +1,54 @@
+package com.springproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User_table")
+public class User {
+	
+	@Id   // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto increment
+	private int id;
+	private String fname;
+	private String lname;
+	private String username;
+	private String password;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
